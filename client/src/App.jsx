09ddefd03460c660c83a839ac839810c5eefc074
jayank-3112 <guptajayank@gmail.com>
2024-04-20 17:51:15 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React , {lazy} from 'react'
+import { BrowserRouter, Routes, Route }  from 'react-router-dom'
+// import Home from './pages/Home'
+
+const Home = lazy(() => import ("./pages/Home"))
+const Login = lazy(() => import ("./pages/Login"))
+const Chat = lazy(() => import ("./pages/Chat"))
+const Group = lazy(() => import ("./pages/Group"))
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/chat/:chatId " element={<Chat/>}/>
+      <Route path="/group" element={<Group/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Paper } from '@mui/material'
+const Login = () => {
+  return (
+    <Container component={"main"} maxWidth="sx">
+        <Paper>
+            elevation={3}
+            
+        </Paper>
+    </Container>
+  )
+}
+
+export default Login
